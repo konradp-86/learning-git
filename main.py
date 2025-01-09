b216643ba3b03c3330b2d7a91ac7c5a7ae298afe
konradp-86 <konradp_86@o2.pl>
2025-01-09 16:38:26 +0100

    rozwiązanie

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def is_prime(num):
     return True
 prime_numbers = [num for num in numbers if is_prime(num)]
 print("Liczby pierwsze:", prime_numbers)
+def print_gwiazdka(lines):
+    gwiazdka = 2
+    for i in range(1, lines + 1):
+        print('*' * gwiazdka)
+        if i % 2 == 0:
+            gwiazdka += 2
+print_gwiazdka(6)
